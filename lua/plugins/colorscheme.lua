b2d37b628a1534c4c9a998b8e4bf887ec2d42bb7
Sølve Monteiro <solve.monteiro@conta.no>
2024-02-15 13:24:32 +0100

    ## - First commit LazyNvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    -- "folke/tokyonight.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      setup = {
+        transparent_background = true,
+      },
+      colorscheme = "catppuccin-frappe",
+      -- colorscheme = "tokyonight-storm",
+    },
+  },
+}
