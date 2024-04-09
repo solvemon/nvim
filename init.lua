5be16dd8d84b20fb3fc3bbcae8784e152bf925a7
solvemon <solvemon@gmail.com>
2024-04-09 08:54:31 +0200

    Long time no commit

--- init.lua
+++ init.lua
@@ -185,13 +185,17 @@ local function live_grep_git_root()
   end
 end
 
+-- LSP shit
+
+vim.keymap.set('n', '<leader>lr', '<CMD>LspRestart<CR>', {desc = "Restart LSP server"})
+
 vim.api.nvim_create_user_command('LiveGrepGitRoot', live_grep_git_root, {})
 
 -- See `:help telescope.builtin
 vim.keymap.set('n', '<leader>bdd', '<CMD>bdelete<CR>', {desc = "Delete current buffer"})
 vim.keymap.set('n', '<leader>bdo', '<CMD>BufferLineCloseOthers<CR>', {desc = "Delete current buffer"})
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+-- vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
